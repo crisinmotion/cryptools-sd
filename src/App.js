16db4d4d043437710d7abca1d3d3e441f7aa0368
Cristopher Caducoy <cristophercaducoy@gmail.com>
2021-07-13 22:38:54 +0800

    [Chore] Updated SD logo and landing page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Cryptools by SteelDemon" />
+        <div style={{margin: '0 auto', width: 150, padding: '10px 20px', backgroundColor: '#ffffff', borderRadius: '50%'}}><img src={logo} alt="Cryptools by SteelDemon" style={{width: '100%', maxWidth: 100}}/></div>
         <h1>Welcome to Cryptools!</h1>
 				<h5>Awesome features coming soon...</h5>
       </header>
